f0691120e94b9e3d096d7369ea25d921e81bcdfc
Chibikuri <ryosuke.satoh.wk@gmail.com>
2022-07-21 22:39:43 -0400

    removed example

--- example/target/debug/deps/example-7b108166cbeaa9d0.d
+++
@@ -1,5 +0,0 @@
-/Users/ryosukesatoh/Projects/RuLa/example/target/debug/deps/example-7b108166cbeaa9d0.rmeta: src/main.rs
-
-/Users/ryosukesatoh/Projects/RuLa/example/target/debug/deps/example-7b108166cbeaa9d0.d: src/main.rs
-
-src/main.rs:
--- example/target/rls/debug/deps/example-1d85313a5781f6ad.d
+++
@@ -1,5 +0,0 @@
-/Users/ryosukesatoh/Projects/RuLa/example/target/rls/debug/deps/example-1d85313a5781f6ad.rmeta: src/main.rs
-
-/Users/ryosukesatoh/Projects/RuLa/example/target/rls/debug/deps/example-1d85313a5781f6ad.d: src/main.rs
-
-src/main.rs:
