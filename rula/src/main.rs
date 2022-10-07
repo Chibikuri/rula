794df3e27d914f00687edbf4f267079ace139b35
Chibikuri <ryosuke.satoh.wk@gmail.com>
2022-10-07 16:41:44 +0900

    updated grammar

--- rula/src/main.rs
+++ rula/src/main.rs
@@ -1,22 +1,38 @@
 use clap::Parser;
+use rula_exec;
+use rula_parser::parse as rula_parse;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(name="rula", author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+    // rula program name
+    rula_program: PathBuf,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    /// Name of the person to greet
+    #[arg(short, long, default_value = "false")]
+    ruleset: bool,
+    // Number of times to greet
+    // #[arg(short, long, default_value_t = 1)]
+    // count: u8,
 }
 
 fn main() {
     let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    let mut rula_program = File::open(args.rula_program).expect("No such file");
+    let mut contents = String::new();
+    rula_program
+        .read_to_string(&mut contents)
+        .expect("Something went wrong reading the file");
+    // parse rula program
+    let ast = rula_parser::parse(&contents).unwrap();
+    println!("{:#?}", ast);
+    if !args.ruleset {
+        println!("Generating RuleSet...");
     }
+    println!("Hello {}!", args.ruleset);
 }
