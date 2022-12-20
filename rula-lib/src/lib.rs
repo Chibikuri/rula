8465106a0bbbd1a59a987a5c0f188b380ceba9a0
Chibikuri <ryosuke.satoh.wk@gmail.com>
2022-12-21 03:11:19 +0900

    cleaned up

--- rula-lib/src/lib.rs
+++ rula-lib/src/lib.rs
@@ -1,10 +1,6 @@
 #![allow(non_snake_case)]
-pub mod message;
 pub mod operation;
 pub mod prelude;
-pub mod qnic;
-pub mod qubit;
-pub mod result;
 pub mod time;
 
 // use ruleset::action::v2::ActionClauses;
