892f47f60e0c728b65a599fce5aea35f5301db51
Chibikuri <ryosuke.satoh.wk@gmail.com>
2023-01-10 14:02:33 +0900

    modified qubit type identifier

--- rula-exec/src/ruleset_gen/tracker.rs
+++ rula-exec/src/ruleset_gen/tracker.rs
@@ -188,39 +188,6 @@ impl Tracker {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Variable {
-    pub name: String,
-    // Currently, this scope is the same as Rule name and RuleSet name
-    pub scope: String,
-    pub variable_kind: VarKind,
-    pub value: Types,
-}
-
-impl Variable {
-    pub fn new(name: &str, scope: &str, variable_kind: VarKind, value: Types) -> Self {
-        Variable {
-            name: name.to_string(),
-            scope: scope.to_string(),
-            variable_kind: variable_kind,
-            value: value,
-        }
-    }
-    pub fn get_scope(&self) -> &String {
-        &self.scope
-    }
-}
-
-// Variable Types with scope
-#[derive(Debug, Clone, PartialEq)]
-pub enum VarKind {
-    Argument,
-    RepeaterArgument,
-    LetAssignment,
-    // Initialization purpose
-    Nil,
-}
-
-#[derive(Debug, Clone, PartialEq)]
 pub struct RetTypeAnnotation {
     // (Type annotation, maybe)
     pub return_types: Vec<(Types, bool)>,
@@ -242,73 +209,5 @@ impl RetTypeAnnotation {
     }
     pub fn get_scope(&self) -> &String {
         &self.scope
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Arguments {
-    // Order is important here
-    pub vals: Vec<RuLaValue>,
-    pub scope: String,
-}
-
-impl Arguments {
-    pub fn new(vals: Vec<RuLaValue>, scope: String) -> Self {
-        Arguments {
-            vals: vals,
-            scope: scope,
-        }
-    }
-    pub fn place_holder() -> Self {
-        Arguments {
-            vals: vec![],
-            scope: String::from(""),
-        }
-    }
-    pub fn get_scope(&self) -> &String {
-        &self.scope
-    }
-    pub fn add_val(&mut self, arg: RuLaValue) {
-        self.vals.push(arg);
-    }
-    pub fn update_scope(&mut self, new_scope: &str) {
-        self.scope = new_scope.to_string();
-    }
-}
-
-// Used to track inside the rule closure
-#[derive(Debug, Clone, PartialEq)]
-pub struct LocalVariableTracker {
-    pub values: RefCell<HashMap<String, RuLaValue>>,
-}
-
-impl LocalVariableTracker {
-    pub fn new() -> Self {
-        LocalVariableTracker {
-            values: RefCell::new(HashMap::new()),
-        }
-    }
-    pub fn register(&self, name: &str, val: &RuLaValue) {
-        self.values
-            .borrow_mut()
-            .insert(name.to_string(), val.clone());
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_clean_scope() {
-        // let mut tracker = Tracker::new();
-        // // Create variables for two scopes
-        // let var_1 = Variable::new("variable", "rule1", VarKind::Argument, Types::Unknown);
-        // let var_2 = Variable::new("variable2", "rule2", VarKind::Argument, Types::Unknown);
-        // tracker.register_local_variable("variable", var_1);
-        // tracker.register_local_variable("variable", var_2);
-        // // clean variables in rule1
-        // tracker.clean_scope("rule1");
-        // assert_eq!(tracker.local_variable.borrow().len(), 1);
     }
 }
